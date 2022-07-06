86a3e771be956eb58e5423229d419b8e65e4a8f1
ashwani kumar <105099645+Ashwanikumar1715@users.noreply.github.com>
2022-07-06 15:40:51 +0530

    Add files via upload

---
+++ javaS.js
@@ -0,0 +1,89 @@
+function effect(){
+    document.getElementById('c').innerHTML=`home for all c program`;
+    document.getElementById('c').style.backgroundColor='red';
+    document.getElementById('c').style.color='white';
+}
+function effectout(){
+    document.getElementById('c').innerHTML=`C language`;
+    document.getElementById('c').style.backgroundColor='aquamarine';
+    document.getElementById('c').style.color='black';
+}
+function cpp(){
+    document.getElementById('cpp').innerHTML=`home for all c++ program`;
+    document.getElementById('cpp').style.backgroundColor='red';
+    document.getElementById('cpp').style.color='white';
+}
+function cppout(){
+    document.getElementById('cpp').innerHTML=`C++`;
+    document.getElementById('cpp').style.backgroundColor='aquamarine';
+    document.getElementById('cpp').style.color='black';
+}
+function java(){
+    document.getElementById('java').innerHTML=`home for all java program`;
+    document.getElementById('java').style.backgroundColor='red';
+    document.getElementById('java').style.color='white';
+}
+function javaout(){
+    document.getElementById('java').innerHTML=`JAVA`;
+    document.getElementById('java').style.backgroundColor='aquamarine';
+    document.getElementById('java').style.color='black';
+}
+function python(){
+    document.getElementById('python').innerHTML=`home for all python program`;
+    document.getElementById('python').style.backgroundColor='red';
+    document.getElementById('python').style.color='white';
+}
+function pythonout(){
+    document.getElementById('python').innerHTML=`PYTHON`;
+    document.getElementById('python').style.backgroundColor='aquamarine';
+    document.getElementById('python').style.color='black';
+}
+
+// ======================================
+// ====================================
+// =================================
+// =============================
+// =========================
+
+
+
+function effectl(){
+    document.getElementById('cl').innerHTML=`theory of c language`;
+    document.getElementById('cl').style.backgroundColor='red';
+    document.getElementById('cl').style.color='white';
+}
+function effectlout(){
+    document.getElementById('cl').innerHTML=`C language`;
+    document.getElementById('cl').style.backgroundColor='aquamarine';
+    document.getElementById('cl').style.color='black';
+}
+function cppl(){
+    document.getElementById('cppl').innerHTML=`theory of c++ language`;
+    document.getElementById('cppl').style.backgroundColor='red';
+    document.getElementById('cppl').style.color='white';
+}
+function cpplout(){
+    document.getElementById('cppl').innerHTML=`C++`;
+    document.getElementById('cppl').style.backgroundColor='aquamarine';
+    document.getElementById('cppl').style.color='black';
+}
+function javal(){
+    document.getElementById('javal').innerHTML=`theory of java language`;
+    document.getElementById('javal').style.backgroundColor='red';
+    document.getElementById('javal').style.color='white';
+}
+function javalout(){
+    document.getElementById('javal').innerHTML=`JAVA`;
+    document.getElementById('javal').style.backgroundColor='aquamarine';
+    document.getElementById('javal').style.color='black';
+}
+function pythonl(){
+    document.getElementById('pythonl').innerHTML=`theory of python language`;
+    document.getElementById('pythonl').style.backgroundColor='red';
+    document.getElementById('pythonl').style.color='white';
+}
+function pythonlout(){
+    document.getElementById('pythonl').innerHTML=`PYTHON`;
+    document.getElementById('pythonl').style.backgroundColor='aquamarine';
+    document.getElementById('pythonl').style.color='black';
+}
